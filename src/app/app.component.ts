a1eed01da7726573203a18f9e209dce99b94c602
Javier <jndominguez@isotrol.com>
2021-03-26 14:50:49 +0100

    event filtering pulsando tecla

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <button (mouseover) = "save($event)">Guardar</button>
+  <input type = "text" (keyup.enter) = "onKeyUp()"/>
   `,
   styles: [`
   `]
 })
 export class AppComponent {
-  save($event){
-    console.log($event);
-  }  
+
+
+  onKeyUp(){
+      console.log("Enter fue presionado");
+  }
 
 }
