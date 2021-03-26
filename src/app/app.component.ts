2d16e93f9c8ed25b53a9b1127f413a8ef0718a2b
Javier <jndominguez@isotrol.com>
2021-03-26 14:28:23 +0100

    class binding en base a una condicion; boton

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <img [src] = " imagenURL"/>
-  <button [disabled] = "botonStatus">Mi boton </button>
+  <button [class.activo] = "isActive">Mi boton</button>
   `,
-  styles: ['']
+  styles: [`
+    .activo{
+      background: green;
+    }
+  `]
 })
 export class AppComponent {
 
   imagenURL = "https://www.chiquipedia.com/images/dibujos-simpsons.jpg?phpMyAdmin=9ea091c51a5aa3cf876fb3cf0a5f7f3d"
-  botonStatus = false;
+  isActive = true;
 
 }
