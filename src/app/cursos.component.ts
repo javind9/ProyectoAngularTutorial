7f2235d750c34df579162af7f6b9896ce085d640
Javier <jndominguez@isotrol.com>
2021-03-26 12:22:38 +0100

    error

---
+++ src/app/cursos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'cursos',
+    template: '<h1>Mis cursos</h1>',
+    styleUrls: ['']
+
+})
+
+export class CursosComponent {
+
+}
