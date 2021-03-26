de23df1eef0713de2d7d78c1029e5e0d8170a45a
Javier <jndominguez@isotrol.com>
2021-03-26 12:41:41 +0100

    Creado un componente

--- src/app/cursos.component.ts
+++ src/app/cursos.component.ts
@@ -2,9 +2,11 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'cursos',
-    template: '<h1>Mis cursos</h1>',
-    styleUrls: ['']
-
+    template: `
+    <h1>Mis cursos</h1>
+    <p>TypeScript</p>
+    `,
+    styles: ['h1 { color: red;}']
 })
 
 export class CursosComponent {
