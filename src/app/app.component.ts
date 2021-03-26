50be835a3078881984a2a4ccf305ffeec3c71445
Javier <jndominguez@isotrol.com>
2021-03-26 14:37:22 +0100

    style binding dependiendo de una variable; ?true, :false

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,17 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <button [class.activo] = "isActive">Mi boton</button>
+  <button [style.borderColor] = "isActive ? 'green' : 'white'">Mi boton</button>
   `,
   styles: [`
-    .activo{
-      background: green;
-    }
   `]
 })
 export class AppComponent {
 
-  imagenURL = "https://www.chiquipedia.com/images/dibujos-simpsons.jpg?phpMyAdmin=9ea091c51a5aa3cf876fb3cf0a5f7f3d"
   isActive = true;
 
 }
