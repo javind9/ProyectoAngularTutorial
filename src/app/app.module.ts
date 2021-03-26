f7b022de50cad60edbc1b0c4c5ae295fd20a3295
Javier <jndominguez@isotrol.com>
2021-03-26 12:58:22 +0100

    Componente utilizando Angular CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CursosComponent } from './cursos.component';
+import { CursosComponent } from './cursos/cursos.component';
 
 @NgModule({
   declarations: [
