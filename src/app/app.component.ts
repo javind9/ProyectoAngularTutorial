5ee4ec3d8992cf95f0c789a33b17aaec15b1333c
Javier <jndominguez@isotrol.com>
2021-04-05 10:48:23 +0200

    Servicios HTTP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UsuariosService } from './usuarios.service';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  persona = {
-    nombre: 'Luis',
-    edad: 20
-  }
+  personas : any = [];
+
+    constructor(){
+        let usuario = new UsuariosService();
+        this.personas = usuario.getUsuarios();
+    }
 }
---
+++ src/app/usuarios.service.ts
@@ -0,0 +1,22 @@
+export class UsuariosService {
+    getUsuarios(){
+        return [
+            {
+                "nombre": "Luis Salinas",
+                "edad": 20,
+                "hobby": 'cantar'
+            },
+            {
+                "nombre": "Day Meyers",
+                "edad": 30,
+                "hobby": 'patinar'
+            },
+            {
+                "nombre": "Aguirre Ellis",
+                "edad": 25,
+                "hobby": 'correr'
+            }
+        ];
+    }
+
+}
