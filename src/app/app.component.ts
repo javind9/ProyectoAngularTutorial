d32a8b7d22bd2cd8dd0bba45a813ce0a45a4da7d
Javier <jndominguez@isotrol.com>
2021-04-05 10:29:34 +0200

    Two way binding ngModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-  <input type = "text" (keyup.enter) = "onKeyUp(nombre.value)"  #nombre/>
-  `,
-  styles: [`
-  `]
+  templateUrl:"./app.component.html",
+  styles: [``]
 })
 export class AppComponent {
 
-
-  onKeyUp(usuario){
-    //Obtenemos el valor ingresado en el textbox.
-      console.log(usuario);
+  persona = {
+    nombre: 'Luis',
+    edad: 20
   }
-
 }
